6f8245a9d3bb1ccd0a07663dd409dc2fbf099bb8
moldedjelly <moldedjelly@gmail.com>
2015-05-08 16:06:26 +1000

    Base

---
+++ craft/config/general.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * General Configuration
+ *
+ * All of your system's general configuration settings go in here.
+ * You can see a list of the default settings in craft/app/etc/config/defaults/general.php
+ */
+
+return array(
+	'*' => array(
+  	'cacheMethod' => 'memcache',
+		'omitScriptNameInUrls' => true,
+		'environmentVariables' => array(
+			'assetsBaseUrl' => '/assets',
+			'assetsBasePath' => './assets',
+		)
+	),
+
+	// Give CodeKit a custom site URL
+	'.local' => array(
+		'siteUrl' => "http://{$_SERVER['SERVER_NAME']}/",
+	)
+);
